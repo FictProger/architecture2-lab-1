ebfe5c641066c0b50377a3387a68219631e144ee
MrILL <ilua2403@ukr.net>
2021-04-11 21:27:03 +0300

    lupa zalupa

--- build/zip_archive/zip_archive.go
+++ build/zip_archive/zip_archive.go
@@ -15,7 +15,7 @@ var (
 
 	// Ninja rule to execute zip.
 	zipRule = pctx.StaticRule("zipArchive", blueprint.RuleParams{
-		Command:     "pwd && ls && zip $outputFile $files",
+		Command:     "zip $outputFile $files",
 		Description: "zipping into $outputFile",
 	}, "workDir", "outputFile", "files")
 )
